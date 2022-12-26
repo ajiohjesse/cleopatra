539c96a87d508cca238c4871ad3b7ef9a997c38a
Jesse Ajioh <ajiohjesse@gmail.com>
2022-12-26 11:39:10 +0000

    add first media queries

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,22 +1,26 @@
 import CircledText from '@components/circledText/CircledText'
 import styles from './Hero.module.css'
 import { IoEnterOutline } from 'react-icons/io5'
+import {FiShoppingCart, FiGift} from 'react-icons/fi'
 
 const Hero = () => {
   return (
     <div className={styles.heroWrapper}>
-      <h1 className={styles.heroTitle}>
-        Wear the <CircledText>best.</CircledText>
-      </h1>
-      <p className={styles.heroDesc}>
-        Get amazing clothing at affordable prices. Look good, feel good.
-      </p>
+      <div className={styles.heroText}>
+        <h1 className={styles.heroTitle}>
+          Wear the <CircledText>best.</CircledText>
+        </h1>
+        <p className={styles.heroDesc}>
+          Get amazing clothing at affordable prices. Look good, feel good.
+        </p>
+      </div>
+      
       <div className={styles.heroBtns}>
         <button>
-          Shop now <IoEnterOutline />
+          Shop now <FiShoppingCart />
         </button>
         <button>
-          See promo <IoEnterOutline />
+          See promo <FiGift />
         </button>
       </div>
     </div>
